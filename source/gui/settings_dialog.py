fbabf458d7e44522d678211b7f8e3682b5267049
daoductrung <trung@ddt.one>
2026-01-19 22:10:01 +0700

    Refactor settings dialog to use tabbed navigation

--- source/gui/settings_dialog.py
+++ source/gui/settings_dialog.py
@@ -14,161 +14,272 @@ languages = {index:language for language, index in enumerate(supported_languages
 class SettingsDialog(wx.Dialog):
 	def __init__(self, parent):
 		wx.Dialog.__init__(self, parent, title=_("Settings"))
-		self.SetSize(500, 500)
+		self.SetSize(600, 450)
 		self.Centre()
 		self.preferences = {}
-		panel = wx.Panel(self)
-		lbl = wx.StaticText(panel, -1, _("Program Language: "), name="language")
-		self.languageBox = wx.Choice(panel, -1, name="language")
+		
+		# --- Main Container ---
+		mainSizer = wx.BoxSizer(wx.VERTICAL)
+		
+		# --- Navigation ---
+		nav_choices = [
+			_("Preferences"), 
+			_("Download"), 
+			_("Player"), 
+			_("Cookies"), 
+			_("Data")
+		]
+		self.navBox = wx.RadioBox(self, label=_("Category"), choices=nav_choices, majorDimension=1, style=wx.RA_SPECIFY_ROWS)
+		self.navBox.Bind(wx.EVT_RADIOBOX, self.onNavChange)
+		
+		# --- Content Container ---
+		self.contentSizer = wx.BoxSizer(wx.VERTICAL)
+		
+		# Create Pages (Panels) hidden by default
+		self.page_prefs = self.setup_prefs_page()
+		self.page_download = self.setup_download_page()
+		self.page_player = self.setup_player_page()
+		self.page_cookies = self.setup_cookies_page()
+		self.page_data = self.setup_data_page()
+		
+		# Add all pages to content sizer
+		self.contentSizer.Add(self.page_prefs, 1, wx.EXPAND | wx.ALL, 5)
+		self.contentSizer.Add(self.page_download, 1, wx.EXPAND | wx.ALL, 5)
+		self.contentSizer.Add(self.page_player, 1, wx.EXPAND | wx.ALL, 5)
+		self.contentSizer.Add(self.page_cookies, 1, wx.EXPAND | wx.ALL, 5)
+		self.contentSizer.Add(self.page_data, 1, wx.EXPAND | wx.ALL, 5)
+		
+		# --- Footer ---
+		footerSizer = wx.BoxSizer(wx.HORIZONTAL)
+		openConfigBtn = wx.Button(self, -1, _("Open Configuration Folder"))
+		okButton = wx.Button(self, wx.ID_OK, _("O&K"))
+		okButton.SetDefault()
+		cancelButton = wx.Button(self, wx.ID_CANCEL, _("C&ancel"))
+		
+		footerSizer.Add(openConfigBtn, 0, wx.ALL, 5)
+		footerSizer.AddStretchSpacer()
+		footerSizer.Add(okButton, 0, wx.ALL, 5)
+		footerSizer.Add(cancelButton, 0, wx.ALL, 5)
+		
+		# Bindings
+		openConfigBtn.Bind(wx.EVT_BUTTON, self.onOpenConfig)
+		okButton.Bind(wx.EVT_BUTTON, self.onOk)
+		self.Bind(wx.EVT_CHAR_HOOK, self.onHook)
+		
+		# Assemble Layout
+		mainSizer.Add(self.navBox, 0, wx.EXPAND | wx.ALL, 5)
+		mainSizer.Add(self.contentSizer, 1, wx.EXPAND | wx.ALL, 5)
+		mainSizer.Add(footerSizer, 0, wx.EXPAND | wx.ALL, 5)
+		
+		self.SetSizer(mainSizer)
+		
+		# Initialize View
+		self.update_pages(0)
+		self.onFormatChange(None) # Init dynamic UI for MP3
+		
+		self.navBox.SetFocus()
+		self.ShowModal()
+
+	def onHook(self, event):
+		key = event.GetKeyCode()
+		# Ctrl+Tab Navigation
+		if key == wx.WXK_TAB and event.ControlDown():
+			sel = self.navBox.GetSelection()
+			count = self.navBox.GetCount()
+			
+			if event.ShiftDown():
+				new_sel = (sel - 1) % count
+			else:
+				new_sel = (sel + 1) % count
+				
+			self.navBox.SetSelection(new_sel)
+			self.update_pages(new_sel)
+			self.navBox.SetFocus()
+			return
+		
+		event.Skip()
+
+	def setup_prefs_page(self):
+		p = wx.Panel(self)
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		
+		# Language (Moved from General)
+		lbl_lang = wx.StaticText(p, -1, _("Program Language: "))
+		self.languageBox = wx.Choice(p, -1, name="language")
 		self.languageBox.Set(list(supported_languages.keys()))
 		try:
 			self.languageBox.Selection = languages[config_get("lang")]
 		except KeyError:
 			self.languageBox.Selection = 0
-		lbl1 = wx.StaticText(panel, -1, _("Download Folder: "), name="path")
-		self.pathField = wx.TextCtrl(panel, -1, value=config_get("path"), name="path", style=wx.TE_READONLY|wx.TE_MULTILINE|wx.HSCROLL)
-		changeButton = wx.Button(panel, -1, _("&Change Path"), name="path")
-		preferencesBox = wx.StaticBox(panel, -1, _("General preferences"))
-		self.autoDetectItem = wx.CheckBox(preferencesBox, -1, _("Auto detect links on startup"), name="autodetect")
-		self.autoCheckForUpdates = wx.CheckBox(preferencesBox, -1, _("Auto check for updates on startup"), name="checkupdates")
-		self.autoLoadItem = wx.CheckBox(preferencesBox, -1, _("Load more results when reaching the end of the list"), name="autoload")
-		self.autoCheckForUpdates.SetValue(config_get("checkupdates"))
+		
+		sizer.Add(lbl_lang, 0, wx.BOTTOM, 5)
+		sizer.Add(self.languageBox, 0, wx.EXPAND | wx.BOTTOM, 15)
+		
+		# Existing Preferences
+		self.autoDetectItem = wx.CheckBox(p, -1, _("Auto detect links on startup"), name="autodetect")
+		self.autoCheckForUpdates = wx.CheckBox(p, -1, _("Auto check for updates on startup"), name="checkupdates")
+		self.autoLoadItem = wx.CheckBox(p, -1, _("Load more results when reaching the end of the list"), name="autoload")
+		self.swapPlayHotkeys = wx.CheckBox(p, -1, _("Swap Enter and Ctrl+Enter for Video/Audio"), name="swap_play_hotkeys")
+		self.speakBackground = wx.CheckBox(p, -1, _("Speak notifications when window is inactive"), name="speak_background")
+		
+		# Values
 		self.autoDetectItem.SetValue(config_get("autodetect"))
+		self.autoCheckForUpdates.SetValue(config_get("checkupdates"))
 		self.autoLoadItem.SetValue(config_get("autoload"))
-		self.swapPlayHotkeys = wx.CheckBox(preferencesBox, -1, _("Swap Enter and Ctrl+Enter for Video/Audio"), name="swap_play_hotkeys")
 		self.swapPlayHotkeys.SetValue(config_get("swap_play_hotkeys"))
-		self.swapPlayHotkeys.SetValue(config_get("swap_play_hotkeys"))
-		self.speakBackground = wx.CheckBox(preferencesBox, -1, _("Speak notifications when window is inactive"), name="speak_background")
 		self.speakBackground.SetValue(config_get("speak_background"))
-		downloadPreferencesBox = wx.StaticBox(panel, -1, _("Download settings"))
-		lbl2 = wx.StaticText(downloadPreferencesBox, -1, _("Direct download format: "))
-		self.formats = wx.Choice(downloadPreferencesBox, -1, choices=[_("Video (mp4)"), _("Audio (m4a)"), _("Audio (mp3)")])
+		
+		# Bindings
+		for ctrl in [self.autoDetectItem, self.autoCheckForUpdates, self.autoLoadItem, self.swapPlayHotkeys, self.speakBackground]:
+			ctrl.Bind(wx.EVT_CHECKBOX, self.onCheck)
+			sizer.Add(ctrl, 0, wx.ALL, 5)
+			
+		p.SetSizer(sizer)
+		return p
+
+	def setup_download_page(self):
+		p = wx.Panel(self)
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		
+		# Download Folder (Moved from General)
+		lbl_path = wx.StaticText(p, -1, _("Download Folder: "))
+		pathSizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.pathField = wx.TextCtrl(p, -1, value=config_get("path"), name="path", style=wx.TE_READONLY|wx.TE_MULTILINE|wx.HSCROLL)
+		changeButton = wx.Button(p, -1, _("&Change Path"))
+		changeButton.Bind(wx.EVT_BUTTON, self.onChange)
+		
+		pathSizer.Add(self.pathField, 1, wx.EXPAND | wx.RIGHT, 5)
+		pathSizer.Add(changeButton, 0)
+		
+		sizer.Add(lbl_path, 0, wx.BOTTOM, 5)
+		sizer.Add(pathSizer, 0, wx.EXPAND | wx.BOTTOM, 15)
+		
+		# Existing Download Settings
+		lbl_fmt = wx.StaticText(p, -1, _("Direct download format: "))
+		self.formats = wx.Choice(p, -1, choices=[_("Video (mp4)"), _("Audio (m4a)"), _("Audio (mp3)")])
 		self.formats.Selection = int(config_get('defaultformat'))
-		self.lblMp3 = wx.StaticText(downloadPreferencesBox, -1, _("MP3 conversion quality: "))
-		self.mp3Quality = wx.Choice(downloadPreferencesBox, -1, choices=["96 kbps", "128 kbps", "192 kbps", "256 kbps", "320 kbps"], name="conversion")
+		self.formats.Bind(wx.EVT_CHOICE, self.onFormatChange)
+		
+		self.lblMp3 = wx.StaticText(p, -1, _("MP3 conversion quality: "))
+		self.mp3Quality = wx.Choice(p, -1, choices=["96 kbps", "128 kbps", "192 kbps", "256 kbps", "320 kbps"], name="conversion")
 		self.mp3Quality.Selection = int(config_get("conversion"))
-		playerOptions = wx.StaticBox(panel, -1, _("Player settings"))
-		self.continueWatching = wx.CheckBox(playerOptions, -1, _("Continue watching"), name="continue")
-		self.continueWatching.Value = config_get("continue")
-		self.repeateTracks = wx.CheckBox(playerOptions, -1, _("Repeat video"), name="repeatetracks")
-		self.autoPlayNext = wx.CheckBox(playerOptions, -1, _("Auto play next video"), name="autonext")
-		self.autoPlayNext.Value = config_get('autonext')
-		self.repeateTracks.Value = config_get("repeatetracks")
-		self.chkSkipSilence = wx.CheckBox(playerOptions, -1, _("Skip silence (Recommended only for music)"), name="skip_silence")
+		
+		sizer.Add(lbl_fmt, 0, wx.TOP, 5)
+		sizer.Add(self.formats, 0, wx.EXPAND | wx.BOTTOM, 15)
+		sizer.Add(self.lblMp3, 0, wx.TOP, 5)
+		sizer.Add(self.mp3Quality, 0, wx.EXPAND | wx.BOTTOM, 5)
+		
+		p.SetSizer(sizer)
+		return p
+
+	def setup_player_page(self):
+		p = wx.Panel(self)
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		
+		self.continueWatching = wx.CheckBox(p, -1, _("Continue watching"), name="continue")
+		self.repeateTracks = wx.CheckBox(p, -1, _("Repeat video"), name="repeatetracks")
+		self.autoPlayNext = wx.CheckBox(p, -1, _("Auto play next video"), name="autonext")
+		self.chkSkipSilence = wx.CheckBox(p, -1, _("Skip silence (Recommended only for music)"), name="skip_silence")
+		self.chkPlayerNotifications = wx.CheckBox(p, -1, _("Speak player status notifications"), name="player_notifications")
+		
+		# Values
+		self.continueWatching.SetValue(config_get("continue"))
+		self.repeateTracks.SetValue(config_get("repeatetracks"))
+		self.autoPlayNext.SetValue(config_get("autonext"))
 		self.chkSkipSilence.SetValue(config_get("skip_silence"))
-		self.chkPlayerNotifications = wx.CheckBox(playerOptions, -1, _("Speak player status notifications"), name="player_notifications")
 		self.chkPlayerNotifications.SetValue(config_get("player_notifications"))
 		
-		cookiesBox = wx.StaticBox(panel, -1, _("YouTube Cookies (Fix Login/Bot errors)"))
+		# Bindings
+		for ctrl in [self.continueWatching, self.repeateTracks, self.autoPlayNext, self.chkSkipSilence, self.chkPlayerNotifications]:
+			ctrl.Bind(wx.EVT_CHECKBOX, self.onCheck)
+			sizer.Add(ctrl, 0, wx.ALL, 5)
+			
+		p.SetSizer(sizer)
+		return p
 
+	def setup_cookies_page(self):
+		p = wx.Panel(self)
+		sizer = wx.BoxSizer(wx.VERTICAL)
 		
-		
-		# Data Management Box
-		dataBox = wx.StaticBox(panel, -1, _("Data Management (Backup/Restore)"))
-		dataSizer = wx.BoxSizer(wx.HORIZONTAL)
-		backupBtn = wx.Button(dataBox, -1, _("Backup Configuration..."))
-		restoreBtn = wx.Button(dataBox, -1, _("Restore Configuration..."))
-		dataSizer.Add(backupBtn, 1, wx.ALL, 5)
-		dataSizer.Add(restoreBtn, 1, wx.ALL, 5)
-		dataBox.SetSizer(dataSizer)
-		
-		# bindings
-		backupBtn.Bind(wx.EVT_BUTTON, self.onBackup)
-		restoreBtn.Bind(wx.EVT_BUTTON, self.onRestore)
+		info = wx.StaticText(p, -1, _("Import cookies.txt to fix login or 403 errors."))
 		
 		btnSizer = wx.BoxSizer(wx.HORIZONTAL)
-		instructionBtn = wx.Button(cookiesBox, -1, _("How to get cookies.txt?"))
-		importCookiesBtn = wx.Button(cookiesBox, -1, _("Import cookies.txt..."))
+		instructionBtn = wx.Button(p, -1, _("How to get cookies.txt?"))
+		importCookiesBtn = wx.Button(p, -1, _("Import cookies.txt..."))
 		
-		btnSizer.Add(instructionBtn, 1, wx.ALL, 5)
-		btnSizer.Add(importCookiesBtn, 1, wx.ALL, 5)
+		btnSizer.Add(instructionBtn, 1, wx.RIGHT, 5)
+		btnSizer.Add(importCookiesBtn, 1)
 		
-		# Clear Cookies Button
-		clearCookiesBtn = wx.Button(cookiesBox, -1, _("Clear cookies.txt"))
-		btnSizer.Add(clearCookiesBtn, 1, wx.ALL, 5)
+		clearCookiesBtn = wx.Button(p, -1, _("Clear cookies.txt"))
 		
 		instructionBtn.Bind(wx.EVT_BUTTON, self.onInstructions)
 		importCookiesBtn.Bind(wx.EVT_BUTTON, self.onImportCookies)
 		clearCookiesBtn.Bind(wx.EVT_BUTTON, self.onClearCookies)
 		
-		# Open Config Button (General)
-		openConfigBtn = wx.Button(panel, -1, _("Open Configuration Folder"))
-		openConfigBtn.Bind(wx.EVT_BUTTON, self.onOpenConfig)
+		sizer.Add(info, 0, wx.ALL, 5)
+		sizer.Add(btnSizer, 0, wx.EXPAND | wx.ALL, 5)
+		sizer.Add(clearCookiesBtn, 0, wx.EXPAND | wx.ALL, 5)
 		
-		self.formats.Bind(wx.EVT_CHOICE, self.onFormatChange)
+		p.SetSizer(sizer)
+		return p
 
-		
-		okButton = wx.Button(panel, wx.ID_OK, _("O&K"), name="ok_cancel")
-		okButton.SetDefault()
-		cancelButton = wx.Button(panel, wx.ID_CANCEL, _("C&ancel"), name="ok_cancel")
+	def setup_data_page(self):
+		p = wx.Panel(self)
 		sizer = wx.BoxSizer(wx.VERTICAL)
-		sizer1 = wx.BoxSizer(wx.HORIZONTAL)
-		sizer2 = wx.BoxSizer(wx.HORIZONTAL)
-		sizer3 = wx.BoxSizer(wx.HORIZONTAL)
-		sizer4 = wx.BoxSizer(wx.VERTICAL)
-		sizer5 = wx.BoxSizer(wx.HORIZONTAL)
-		sizer6 = wx.BoxSizer(wx.HORIZONTAL)
-		sizer7 = wx.BoxSizer(wx.HORIZONTAL)
-		okCancelSizer = wx.BoxSizer(wx.HORIZONTAL)
-		sizer1.Add(lbl, 1)
-		sizer1.Add(self.languageBox, 1, wx.EXPAND)
-		for control in panel.GetChildren():
-			if control.Name == "ok_cancel":
-				okCancelSizer.Add(control, 1)
-			elif control.Name == "path":
-				sizer2.Add(control, 1)
-		for item in preferencesBox.GetChildren():
-			sizer3.Add(item, 1)
-		preferencesBox.SetSizer(sizer3)
-		sizer5.Add(self.lblMp3, 1)
-		sizer5.Add(self.mp3Quality, 1)
-		sizer6.Add(lbl2, 1)
-		sizer6.Add(self.formats, 1)
-		sizer4.Add(sizer5)
-		sizer4.Add(sizer6)
-		downloadPreferencesBox.SetSizer(sizer4)
-		for ctrl in playerOptions.GetChildren():
-			sizer7.Add(ctrl, 1)
-		playerOptions.SetSizer(sizer7)
-		sizer.Add(sizer1, 1, wx.EXPAND)
-		sizer.Add(sizer2, 1, wx.EXPAND)
-		sizer.Add(preferencesBox, 1, wx.EXPAND)
-		sizer.Add(downloadPreferencesBox, 1, wx.EXPAND)
-		sizer.Add(playerOptions, 1, wx.EXPAND)
-		sizer.Add(cookiesBox, 1, wx.EXPAND)
-		sizer.Add(dataBox, 1, wx.EXPAND)
 		
-		# Cookie Sizer
-		cookieSizer = wx.BoxSizer(wx.VERTICAL)
-		cookieSizer.Add(btnSizer, 0, wx.EXPAND)
-		cookiesBox.SetSizer(cookieSizer)
+		info = wx.StaticText(p, -1, _("Backup or Restore your configuration."))
 		
-		okCancelSizer.Add(openConfigBtn, 1) # Add Open Config to bottom row
-		sizer.Add(okCancelSizer, 1, wx.EXPAND)
-		panel.SetSizer(sizer)
-		changeButton.Bind(wx.EVT_BUTTON, self.onChange)
-		self.autoDetectItem.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.autoLoadItem.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.autoCheckForUpdates.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.repeateTracks.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.autoPlayNext.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.continueWatching.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.swapPlayHotkeys.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.speakBackground.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.chkSkipSilence.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		self.chkPlayerNotifications.Bind(wx.EVT_CHECKBOX, self.onCheck)
-		okButton.Bind(wx.EVT_BUTTON, self.onOk)
+		dataSizer = wx.BoxSizer(wx.HORIZONTAL)
+		backupBtn = wx.Button(p, -1, _("Backup Configuration..."))
+		restoreBtn = wx.Button(p, -1, _("Restore Configuration..."))
 		
-		# Initial state check
-		self.onFormatChange(None)
+		dataSizer.Add(backupBtn, 1, wx.RIGHT, 5)
+		dataSizer.Add(restoreBtn, 1)
 		
-		self.ShowModal()
+		backupBtn.Bind(wx.EVT_BUTTON, self.onBackup)
+		restoreBtn.Bind(wx.EVT_BUTTON, self.onRestore)
+		
+		sizer.Add(info, 0, wx.ALL, 5)
+		sizer.Add(dataSizer, 0, wx.EXPAND | wx.ALL, 5)
+		
+		p.SetSizer(sizer)
+		return p
+
+	def onNavChange(self, event):
+		selection = self.navBox.GetSelection()
+		self.update_pages(selection)
+
+	def update_pages(self, selection):
+		# Hide all pages first
+		self.page_prefs.Hide()
+		self.page_download.Hide()
+		self.page_player.Hide()
+		self.page_cookies.Hide()
+		self.page_data.Hide()
+		
+		# Show selected
+		target = None
+		if selection == 0: target = self.page_prefs
+		elif selection == 1: target = self.page_download
+		elif selection == 2: target = self.page_player
+		elif selection == 3: target = self.page_cookies
+		elif selection == 4: target = self.page_data
+		
+		if target:
+			target.Show()
+			# target.SetFocus() # Removed to keep focus on NavBox
+			
+		self.Layout()
+
 	def onFormatChange(self, event):
-		# "Audio (mp3)" is index 2, check self.formats choices
+		# "Audio (mp3)" is index 2
 		is_mp3 = (self.formats.Selection == 2)
+		# Toggle visibility on Download Page Controls
 		self.lblMp3.Show(is_mp3)
 		self.mp3Quality.Show(is_mp3)
+		self.page_download.Layout() # Re-layout the page
 		self.Layout()
+
 	def onCheck(self, event):
 		obj = event.EventObject
 		if all((self.repeateTracks.Value, self.autoPlayNext.Value)) and obj in (self.repeateTracks, self.autoPlayNext):
@@ -200,11 +311,9 @@ class SettingsDialog(wx.Dialog):
 			except Exception as e:
 				wx.MessageBox(_("Failed to import cookies: {}").format(e), _("Error"), parent=self, style=wx.ICON_ERROR)
 		dlg.Destroy()
-
 	def onInstructions(self, event):
 		msg = _("1. Install 'Get cookies.txt LOCALLY' extension for Chrome/Firefox.\n2. Open YouTube and log in.\n3. Open the extension and click 'Export'.\n4. Save the file.\n5. Click 'Import cookies.txt...' below and select that file.")
 		wx.MessageBox(msg, _("Instructions"), parent=self)
-
 	def onClearCookies(self, event):
 		target = os.path.join(settings_path, "cookies.txt")
 		if os.path.exists(target):
@@ -216,13 +325,11 @@ class SettingsDialog(wx.Dialog):
 					wx.MessageBox(_("An error occurred: {}").format(e), _("Error"), parent=self, style=wx.ICON_ERROR)
 		else:
 			wx.MessageBox(_("No cookies found to delete."), _("Info"), parent=self)
-
 	def onOpenConfig(self, event):
 		try:
 			os.startfile(settings_path)
 		except Exception as e:
 			wx.MessageBox(_("An error occurred: {}").format(e), _("Error"), parent=self, style=wx.ICON_ERROR)
-
 	def onBackup(self, event):
 		dlg = wx.FileDialog(self, _("Save Backup File"), wildcard="ZIP files (*.zip)|*.zip", style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
 		if dlg.ShowModal() == wx.ID_OK:
@@ -235,7 +342,6 @@ class SettingsDialog(wx.Dialog):
 			else:
 				wx.MessageBox(_("Backup failed."), _("Error"), parent=self, style=wx.ICON_ERROR)
 		dlg.Destroy()
-		
 	def onRestore(self, event):
 		dlg = wx.FileDialog(self, _("Select Backup File"), wildcard="ZIP files (*.zip)|*.zip", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
 		if dlg.ShowModal() == wx.ID_OK:
@@ -249,22 +355,16 @@ class SettingsDialog(wx.Dialog):
 				else:
 					wx.MessageBox(_("Restore failed."), _("Error"), parent=self, style=wx.ICON_ERROR)
 		dlg.Destroy()
-
 	def onOk(self, event):
-
 		from settings_handler import config_set, config_update_many
 		
-		restart = False # Initialize variable
-
+		restart = False 
 		if self.preferences:
 			config_update_many(self.preferences)
 		if not self.mp3Quality.Selection == int(config_get("conversion")):
 			config_set("conversion", self.mp3Quality.Selection)
 		config_set("defaultformat", self.formats.Selection) if not self.formats.Selection == int(config_get('defaultformat')) else None
-		# Check Skip Silence Change
-		# if "skip_silence" in self.preferences:
-		# 	restart = True # No longer needed with dynamic media options
-			
+		
 		lang = {value:key for key, value in languages.items()}
 		if not lang[self.languageBox.Selection] == config_get("lang"):
 			config_set("lang", lang[self.languageBox.Selection])
@@ -274,5 +374,4 @@ class SettingsDialog(wx.Dialog):
 			msg = wx.MessageBox(_("Configuration changed. You must restart the program for the changes to take effect. Do you want to restart now?"), _("Alert"), style=wx.YES_NO | wx.ICON_WARNING, parent=self)
 			if msg == 2: # wx.YES
 				os.execl(sys.executable, sys.executable, *sys.argv)
-
 		self.Destroy()
